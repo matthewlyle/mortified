eb00f388ec76bdbbe64388ec92961868b0eed652
Matt Dunn <matt@processyellow.ca>
2017-04-25 12:29:08 -0400

    tighten spacing on header and change nav links

--- header.php
+++ header.php
@@ -19,15 +19,15 @@
 
 <body <?php body_class(); ?>>
 
-	<header class="m-top-large container c-red t-white p-large t-shadow ">
+	<header class="m-top-large container c-red t-white p t-shadow ">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="http://getmortified.com/wp-content/uploads/2015/02/nav-logo1.png" class="siteLogo" style="max-width: 250px" /></a>
 		<a href="#" class="btn t-white responsiveNavTrigger"><i class="fa fa-bars"></i> Menu</a>
 		<nav>
 			<ul class="siteNav m-bottom-large">
-				<li><a href="/attend" class="siteNav__item <?php if (is_page('Attend Mortified')) {echo " is-active"; }?>">Attend <i class="fa fa-ticket" aria-hidden="true"></i></a></li>
-				<li><a href="/podcast" class="siteNav__item<?php if (is_page('Listen')) {echo " is-active"; }?>">Listen <i class="fa fa-headphones" aria-hidden="true"></i></a></li>
+				<li><a href="/live" class="siteNav__item <?php if (is_page('Live Events')) {echo " is-active"; }?>">Live <i class="fa fa-ticket" aria-hidden="true"></i></a></li>
 				<li><a href="/participate" class="siteNav__item<?php if (is_page('Participate')) {echo " is-active"; }?>">Participate <i class="fa fa-microphone" aria-hidden="true"></i></a></li>
-				<li><a href="/watch/mortified-nation" class="siteNav__item<?php if (is_page('Mortified Nation')) {echo " is-active"; }?>">Watch <i class="fa fa-television" aria-hidden="true"></i></a></li>
+				<li><a href="/podcast" class="siteNav__item<?php if (is_page('Listen')) {echo " is-active"; }?>">Podcast <i class="fa fa-headphones" aria-hidden="true"></i></a></li>
+				<li><a href="/watch/mortified-nation" class="siteNav__item<?php if (is_page('Mortified Nation')) {echo " is-active"; }?>">Screen <i class="fa fa-television" aria-hidden="true"></i></a></li>
 				<li><a href="/books" class="siteNav__item<?php if (is_page('Books')) {echo " is-active"; }?>">Books <i class="fa fa-book" aria-hidden="true"></i></a></li>
 			</ul>
 		</nav>
