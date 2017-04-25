f87f16551106096850daf7e3524127c80b85fdc8
Matt Dunn <matt@processyellow.ca>
2017-04-25 12:28:54 -0400

    update footer styles:

--- footer.php
+++ footer.php
@@ -13,39 +13,39 @@
 
 </div><!-- #content -->
 
-<footer class="container c-black-dark m-bottom-large p-top-large t-centered">
+<footer class="container c-black-dark p-top t-centered" style="margin-bottom: 130px;">
 	<div class="container">
 		<section class="g-1_4">
-			<h3 class="t-white m-bottom">About Mortified</h3>
-			<ul class="list-unset m-bottom-large">
-				<li><a href="/about" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">About</a></li>
-				<li><a href="/faq" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">FAQ</a></li>
-				<li><a href="/newsletter" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Newsletter</a></li>
+			<h3 class="t-white">About Mortified</h3>
+			<ul class="list-unset m-bottom">
+				<li><a href="/about" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">About</a></li>
+				<li><a href="/faq" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">FAQ</a></li>
+				<li><a href="/newsletter" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Newsletter</a></li>
 				<li><a href="/contact" class="t-dark-grey block t-small">Contact</a></li>
 			</ul>
 		</section>
 		<section class="g-1_4">
-			<h3 class="t-white m-bottom">Team up with us</h3>
-			<ul class="list-unset m-bottom-large">
-				<li><a href="/participate" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Participate</a></li>
-				<li><a href="/start-a-chapter" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Start a Chapter</a></li>
-				<li><a href="/about/opp" class="t-dark-grey block p-bottom-small m-bottom-small t-small">Opportunities &amp; Internships</a></li>
+			<h3 class="t-white">Team Up With Us</h3>
+			<ul class="list-unset m-bottom">
+				<li><a href="/participate" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Participate</a></li>
+				<li><a href="/start-a-chapter" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Start a Chapter</a></li>
+				<li><a href="/about/opp" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Opportunities &amp; Internships</a></li>
 			</ul>
 		</section>
 		<section class="g-1_4">
-			<h3 class="t-white">Other stuff</h3>
-			<ul class="list-unset m-bottom-large">
-				<li><a href="/muse" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Art Inspired by the Podcast</a></li>
-				<li><a href="http://www.chopshopstore.com/products/mortified-id-tee?variant=15336608643%20target=" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">T-Shirts</a></li>
-				<li><a href="/dry-hump-etiquette" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Dry Hump Etiquette</a></li>
+			<h3 class="t-white">Other Stuff</h3>
+			<ul class="list-unset m-bottom">
+				<li><a href="/muse" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Art Inspired by the Podcast</a></li>
+				<li><a href="http://www.chopshopstore.com/products/mortified-id-tee?variant=15336608643%20target=" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">T-Shirts</a></li>
+				<li><a href="/dry-hump-etiquette" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Dry Hump Etiquette</a></li>
 			</ul>
 		</section>
 		<section class="g-1_4">
 			<h3 class="t-white">Languages</h3>
-			<ul class="list-unset m-bottom-large">
-				<li><a href="/france" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">French</a></li>
-				<li><a href="/norway" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Norweigien</a></li>
-				<li><a href="http://www.skambyran.se/" class="t-dark-grey block p-bottom-small m-bottom-small t-small border-bottom-dark">Swedish</a></li>
+			<ul class="list-unset m-bottom">
+				<li><a href="/france" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">French</a></li>
+				<li><a href="/norway" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Norweigien</a></li>
+				<li><a href="http://www.skambyran.se/" class="t-dark-grey block p-bottom-smaller m-bottom-small t-small">Swedish</a></li>
 				<li><a href="/netherlands" class="t-dark-grey block t-small">Dutch</a></li>
 			</ul>
 		</section>
